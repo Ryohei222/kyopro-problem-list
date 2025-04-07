43fb8cfca5d4fd61d5dc43390b4b9d79c61d5fd8
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-08 05:58:20 +0900

    feat: users can save their Ids

---
+++ prisma/migrations/20250407171725_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "ProblemProvider" ADD VALUE 'MOFE';
---
+++ prisma/migrations/20250407184932_/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "AOJId" TEXT,
+ADD COLUMN     "AtCoderId" TEXT,
+ADD COLUMN     "BlogURL" TEXT,
+ADD COLUMN     "CodeforcesId" TEXT,
+ADD COLUMN     "GitHubId" TEXT,
+ADD COLUMN     "MOFEId" TEXT,
+ADD COLUMN     "XId" TEXT,
+ADD COLUMN     "YukicoderId" TEXT,
+ADD COLUMN     "displayName" TEXT;
