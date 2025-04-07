f509fcb54785f5ed740e78477bfe670ce0a9f654
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-08 06:02:54 +0900

    feat: add image remote patterns for GitHub avatars

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ module.exports = {
     eslint: {
         dirs: ["src"], // 本番ビルド（next build）時には'pages'と'utils'ディレクトリのみでESLintを実行する
     },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
