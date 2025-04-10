12c9168a7bc9799810ece214625a574de1422177
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-10 09:15:07 +0900

    chore: install jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,29 @@ const eslintConfig = [
     ...compat.config({
         extends: ["next/core-web-vitals", "next", "prettier"],
     }),
+    {
+        rules: {
+            "import/no-restricted-paths": [
+                "error",
+                {
+                    zones: [
+                        //profile feature以外でtargetのpathをインポートできないように
+                        {
+                            from: "./src/features/!(profile)/**/*", // 対象の依存パス
+                            target: "./src/features/profile", // 禁止するパス
+                            message: "features同士で依存することはできません", // エラーメッセージ
+                        },
+                        //message feature以外でtargetのpathをインポートできないように
+                        {
+                            from: "./src/features/!(message)/**/*", // 対象の依存パス
+                            target: "./src/features/message", // 禁止するパス
+                            message: "features同士で依存することはできません", // エラーメッセージ
+                        },
+                    ],
+                },
+            ],
+        },
+    },
 ];
 
 export default eslintConfig;
