0794ed94824714df1225af37f81107c0d802d243
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-12 01:46:54 +0900

    feat: add checkbox in edit problen list

--- src/features/problemlist/components/ProblemSetIsPublicInput.tsx
+++ src/features/problemlist/components/ProblemSetIsPublicInput.tsx
@@ -15,8 +15,27 @@ export default function ProblemSetIsPublicInput({
                 id="isPublic"
                 checked={isPublic}
                 onCheckedChange={handleIsPublicChange}
-                className="border border-gray-300 rounded-sm w-4 h-4"
-            />
+                className={`border rounded-md w-5 h-5 flex items-center justify-center transition-colors duration-200  ${
+                    isPublic
+                        ? "border-green-500 bg-green-100 text-green-700"
+                        : "border-gray-300 bg-white text-gray-500"
+                }`}
+            >
+                {isPublic && (
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 00-1.414 0L8 12.586 4.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l8-8a1 1 0 000-1.414z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                )}
+            </Checkbox>
             <Label htmlFor="isPublic" className="text-sm text-gray-700">
                 公開する
             </Label>
