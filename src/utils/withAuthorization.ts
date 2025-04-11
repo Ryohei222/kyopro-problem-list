5efcea201670cc4276aad39bdbead4a355fa6f35
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-12 01:44:40 +0900

    refactor: simplify argument handling in withAuthorization

--- src/utils/withAuthorization.ts
+++ src/utils/withAuthorization.ts
@@ -2,22 +2,15 @@ import { auth } from "@/auth";
 import { RequestedUserId } from "@/types/RequestedUserId";
 import { redirect } from "next/navigation";
 
-export type ArgumentsWithoutRequestedUserId<T extends (...args: any[]) => any> = T extends (
-    requestedUserId: RequestedUserId,
-    ...args: infer R
-) => any
-    ? R
-    : never;
+type OmitFirstArg<F> = F extends (x: RequestedUserId, ...args: infer P) => any ? P : never;
 
-export function withAuthorization<
-    T extends (requestedUserId: RequestedUserId, ...args: any[]) => ReturnType<T>,
->(callback: T) {
-    return async function (...args: ArgumentsWithoutRequestedUserId<T>) {
+export function withAuthorization<F extends (...args: any[]) => any>(callback: F) {
+    return async function (...args: OmitFirstArg<F>) {
         const session = await auth();
         const requestedUserId = session?.user?.id;
         if (!requestedUserId) {
             redirect("/");
         }
-        return callback(requestedUserId as RequestedUserId, args);
+        return callback(requestedUserId as RequestedUserId, ...args);
     };
 }
