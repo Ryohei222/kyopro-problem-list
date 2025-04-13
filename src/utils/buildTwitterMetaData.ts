7f0669ebba02d940cf559bd24e74a162be26bc73
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-13 09:04:06 +0900

    feat: add utility function for build twitter metadata

---
+++ src/utils/buildTwitterMetaData.ts
@@ -0,0 +1,16 @@
+import { Twitter } from "next/dist/lib/metadata/types/twitter-types";
+
+type BuildTwitterMetadataProps = {
+    title?: string;
+    description: string;
+};
+
+export default function buildTwitterMetadata(props: BuildTwitterMetadataProps): Twitter {
+    const { title, description } = props;
+    const titleWithPrefix = !title ? "Kyopro Problem List" : title + " | Kyopro Problem List";
+    return {
+        card: "summary",
+        title: titleWithPrefix,
+        description,
+    };
+}
