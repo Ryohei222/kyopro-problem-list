428554a2a084f681e78ebb1fa39e83e8d373d8a1
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-16 07:35:49 +0900

    feat: enhance updateProblems function to handle updates and additions

--- src/db/updateProblems.ts
+++ src/db/updateProblems.ts
@@ -7,22 +7,52 @@ export default async function updateProblems(
 ): Promise<CreatedProblem[]> {
 	const dbProblems = await prisma.problem.findMany({
 		select: {
+			id: true,
 			resource: true,
 			contestId: true,
 			problemId: true,
 			name: true,
+			contestName: true,
+			difficulty: true,
 		},
 	});
 
-	const dbProblemSet = new Set(
-		dbProblems.map((problem) => createProblemKey(problem)),
+	const dbProblemMap = new Map(
+		dbProblems.map((problem) => [createProblemKey(problem), problem]),
 	);
 
 	const createdProblems = await prisma.problem.createManyAndReturn({
 		data: problems.filter(
-			(problem) => !dbProblemSet.has(createProblemKey(problem)),
+			(problem) => !dbProblemMap.has(createProblemKey(problem)),
 		),
 	});
 
-	return createdProblems;
+	const updatedProblems = await prisma.$transaction(
+		problems
+			.map((problem) => {
+				const dbProblem = dbProblemMap.get(createProblemKey(problem));
+				if (
+					!dbProblem ||
+					!(
+						dbProblem.name !== problem.name ||
+						dbProblem.difficulty !== problem.difficulty ||
+						dbProblem.contestName !== problem.contestName
+					)
+				) {
+					return false;
+				}
+
+				return prisma.problem.update({
+					where: { id: dbProblem.id },
+					data: {
+						name: problem.name,
+						difficulty: problem.difficulty,
+						contestName: problem.contestName,
+					},
+				});
+			})
+			.filter((problem) => problem !== false),
+	);
+
+	return [...createdProblems, ...updatedProblems];
 }
