6ebc8e06a937140441d6f5abc369b0a2421ec266
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-17 05:19:15 +0900

    refactor: remove SessionProvider from RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { SWRProvider } from "@/hooks/SWRProvider";
 import buildTwitterMetadata from "@/utils/buildTwitterMetaData";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import type { Metadata } from "next";
-import { SessionProvider } from "next-auth/react";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
@@ -35,16 +34,14 @@ export default async function RootLayout({
 	return (
 		<html lang="ja">
 			<SWRProvider>
-				<SessionProvider>
-					<body
-						className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-					>
-						<div className="min-h-screen bg-gray-50">
-							<ToolBar />
-							<main className="container mx-auto py-6 px-4">{children}</main>
-						</div>
-					</body>
-				</SessionProvider>
+				<body
+					className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				>
+					<div className="min-h-screen bg-gray-50">
+						<ToolBar />
+						<main className="container mx-auto py-6 px-4">{children}</main>
+					</div>
+				</body>
 			</SWRProvider>
 			{process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID &&
 				process.env.NODE_ENV === "production" && (
