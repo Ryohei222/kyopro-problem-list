f9966bbf11c92a3d5290a2d3bf15ee2d262ac009
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-18 01:14:46 +0900

    feat: add dialog when user deletes problem lists

--- src/app/problemlist/show/_components/DeleteButton.tsx
+++ src/app/problemlist/show/_components/DeleteButton.tsx
@@ -1,5 +1,17 @@
 "use client";
 
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+	AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { deleteProblemList } from "@/features/problemlist/db/deleteProblemList";
 import { Trash2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import type { ProblemListResponse } from "../../../../features/problemlist/types/ProblemLists";
@@ -12,11 +24,41 @@ type DeleteButtonProps = {
 export function DeleteButton(props: DeleteButtonProps) {
 	const router = useRouter();
 	return (
-		<CardHeaderButton
-			label="削除"
-			lucideIcon={Trash2}
-			variant="destructive"
-			onClick={() => router.push(`/problemlist/delete/${props.problemList.id}`)}
-		/>
+		<AlertDialog>
+			<AlertDialogTrigger asChild>
+				<CardHeaderButton
+					lucideIcon={Trash2}
+					label="削除"
+					variant="destructive"
+					onClick={() => {}}
+				/>
+			</AlertDialogTrigger>
+			<AlertDialogContent>
+				<AlertDialogHeader>
+					<AlertDialogTitle>
+						本当に問題リスト「{props.problemList.name}」を削除しますか？
+					</AlertDialogTitle>
+					<AlertDialogDescription>
+						削除した問題リストは復元できません。
+						<br />
+						この問題リストには {props.problemList.problemListRecords.length}{" "}
+						個の問題が含まれ、{props.problemList.stars.length}{" "}
+						人がお気に入りに登録しています。
+					</AlertDialogDescription>
+				</AlertDialogHeader>
+				<AlertDialogFooter>
+					<AlertDialogCancel>キャンセル</AlertDialogCancel>
+					<AlertDialogAction
+						onClick={() =>
+							deleteProblemList(props.problemList.id).then(() =>
+								router.push("/"),
+							)
+						}
+					>
+						削除する
+					</AlertDialogAction>
+				</AlertDialogFooter>
+			</AlertDialogContent>
+		</AlertDialog>
 	);
 }
