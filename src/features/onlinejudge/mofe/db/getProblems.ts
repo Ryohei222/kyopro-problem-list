ff7b3edde8482d72bf50e8ed3c932809916f4d13
Ryohei Kobayashi <kobaryo222912@gmail.com>
2025-04-21 10:42:19 +0900

    format

--- src/features/onlinejudge/mofe/db/getProblems.ts
+++ src/features/onlinejudge/mofe/db/getProblems.ts
@@ -2,13 +2,8 @@ import { prisma } from "@/prisma";
 import { MofeProblem } from "../Problem";
 
 export async function getMofeProblems(): Promise<MofeProblem[]> {
-  const entries = await prisma.mofeProblem.findMany({});
-  return entries.map(
-    (d) => new MofeProblem(
-      d.slug,
-      d.contestSlug,
-      d.name,
-      d.contestName
-    )
-  );
+	const entries = await prisma.mofeProblem.findMany({});
+	return entries.map(
+		(d) => new MofeProblem(d.slug, d.contestSlug, d.name, d.contestName),
+	);
 }
